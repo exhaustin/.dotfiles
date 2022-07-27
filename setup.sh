62adb8c921132ded0d1e7fb18a46772a63f22107
ExhAustin <aswang@andrew.cmu.edu>
2022-07-27 12:58:14 -0700

    Add ssh key setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 # Install cmd line tools
 sudo apt install vim tmux htop zsh fonts-powerline
 
+# Generate ssh key for GitHub
+ssh-keygen -t ed25519 -C "austinswang@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+echo "Add the following public key to GitHub: "
+cat ~/.ssh/id_ed25519.pub
+
 # Copy configs
 cp -r ~/.dotfiles/copy_contents_to_home/. ~
 
