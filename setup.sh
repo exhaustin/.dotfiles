6d36fe9d0c12fc242d8eabce1522d6e9a49bd782
ExhAustin <aswang@andrew.cmu.edu>
2023-03-22 09:49:37 -0700

    Use mambaforge instead of conda

--- setup.sh
+++ setup.sh
@@ -15,9 +15,5 @@ cp -r ~/.dotfiles/copy_contents_to_home/. ~
 chsh -s $(which zsh)
 
 # Install miniconda
-mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -rf ~/miniconda3/miniconda.sh
-~/miniconda3/bin/conda init bash
-~/miniconda3/bin/conda init zsh
+curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
+bash Mambaforge-$(uname)-$(uname -m).sh
