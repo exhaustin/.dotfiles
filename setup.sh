8542440dc0d5a41c66595cf51612ef07d55b1791
Austin Wang <austin@Sunnys-MacBook-Air.local>
2024-02-02 22:18:38 -0800

    Remove starship

--- setup.sh
+++ setup.sh
@@ -16,6 +16,3 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 chsh -s $(which zsh)
-
-# Install Starship
-curl -sS https://starship.rs/install.sh | sh
