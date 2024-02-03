51d81ea2da66b16d46511752144856280e9b2f90
Austin Wang <austin@Sunnys-MacBook-Air.local>
2024-02-02 21:50:50 -0800

    Update setup procedure

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo apt install vim tmux htop zsh fonts-powerline
 ssh-keygen -t ed25519 -C "austinswang@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
-echo "Add the following public key to GitHub: "
+echo "Add the following public key to GitHub/GitLab: "
 cat ~/.ssh/id_ed25519.pub
 
 # Copy configs
@@ -13,7 +13,3 @@ cp -r ~/.dotfiles/copy_contents_to_home/. ~
 
 # Set shell to zsh
 chsh -s $(which zsh)
-
-# Install miniconda
-curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
-bash Mambaforge-$(uname)-$(uname -m).sh
